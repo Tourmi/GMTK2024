c5ecb38484218fcedb892dd132d4bbf7b0609a10
Tourmi <ragtag_devilstick@tourmi.dev>
2024-08-17 03:56:43 -0400

    progress

--- Assets/Scripts/Components/PathGenerator.cs
+++ Assets/Scripts/Components/PathGenerator.cs
@@ -7,7 +7,21 @@ using UnityEngine.Tilemaps;
 public class PathGenerator : MonoBehaviour
 {
     [SerializeField]
-    private Vector2Int _extents;
+    [Range(1, 50)]
+    private int _extents;
+
+    [SerializeField]
+    [Range(1, 50)]
+    private int _excludeFrontRange;
+    [SerializeField]
+    [Range(1, 50)]
+    private int _excludeBackRange;
+    [SerializeField]
+    [Range(1, 50)]
+    private int _excludeRightRange;
+    [SerializeField]
+    [Range(1, 50)]
+    private int _excludeLeftRange;
 
     [SerializeField]
     private TileBase _solidTile;
@@ -31,22 +45,83 @@ public class PathGenerator : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLineStrip(new Vector3[]
+        {
+            transform.position + new Vector3(-_extents, 0, -_extents),
+            transform.position + new Vector3(-_extents, 0, _extents),
+            transform.position + new Vector3(_extents, 0, _extents),
+            transform.position + new Vector3(_extents, 0, -_extents),
+        }, true);
+        Gizmos.DrawLineStrip(new Vector3[]
+        {
+            transform.position + new Vector3(-_extents, 1, -_extents),
+            transform.position + new Vector3(-_extents, 1, _extents),
+            transform.position + new Vector3(_extents, 1, _extents),
+            transform.position + new Vector3(_extents, 1, -_extents),
+        }, true);
+        Gizmos.DrawLine(transform.position + new Vector3(-_extents, 0, -_extents), transform.position + new Vector3(-_extents, 1, -_extents));
+        Gizmos.DrawLine(transform.position + new Vector3(-_extents, 0, _extents), transform.position + new Vector3(-_extents, 1, _extents));
+        Gizmos.DrawLine(transform.position + new Vector3(_extents, 0, _extents), transform.position + new Vector3(_extents, 1, _extents));
+        Gizmos.DrawLine(transform.position + new Vector3(_extents, 0, -_extents), transform.position + new Vector3(_extents, 1, -_extents));
+
+        Gizmos.color = new Color(1, 0.5f, 0);
+        var frontSigns = new Vector2Int(1, -1);
+        var backSigns = new Vector2Int(-1, 1);
+        var rightSigns = new Vector2Int(1, 1);
+        var leftSigns = new Vector2Int(-1, -1);
+        DrawExclusionCubes(_excludeFrontRange, frontSigns);
+        DrawExclusionCubes(_excludeBackRange, backSigns);
+        DrawExclusionCubes(_excludeRightRange, rightSigns);
+        DrawExclusionCubes(_excludeLeftRange, leftSigns);
+    }
+
+    void DrawExclusionCubes(int range, Vector2Int signs)
+    {
+        var centerX = signs.x * range + 0.5f;
+        var centerY = signs.y * range + 0.5f;
+        DrawExclusionCube(centerX, centerY);
+        for (var i = 1; i <= _extents - range; i++)
+        {
+            DrawExclusionCube(centerX - i * signs.x, centerY + i * signs.y);
+            DrawExclusionCube(centerX + (i * signs.x), centerY - i * signs.y);
+        }
+    }
+
+    void DrawExclusionCube(float x, float z)
+    {
+        Gizmos.DrawCube(transform.position + new Vector3(x, 0.5f, z), Vector3.one);
+    }
+
     void Regenerate()
     {
-        _tilemap.CompressBounds();
-
         foreach (var child in transform.Cast<Transform>().ToArray())
         {
             DestroyImmediate(child.gameObject);
         }
 
-        var position = new Vector3Int(-_extents.x, -_extents.y, 0);
-        var size = new Vector3Int(_extents.x * 2, _extents.y * 2, 1);
+        _tilemap.ClearAllTiles();
+
+        var position = new Vector3Int(-_extents, -_extents, 0);
+        var size = new Vector3Int(_extents * 2 + 1, _extents * 2 + 1, 1);
         var bounds = new BoundsInt(position, size);
 
-        var gridPositions = bounds.allPositionsWithin.Collect().Where(p => Random.Range(0f, 1f) < 0.5).ToArray();
-        _tilemap.ClearAllTiles();
+        var gridPositions = bounds.allPositionsWithin
+            .Collect()
+            .Where(p => Random.Range(0f, 1f) < 0.5)
+            .Where(IsInsideInclusionZone)
+            .ToArray();
+        var outsidePositions = bounds.allPositionsWithin
+            .Collect()
+            .Where(p => !IsInsideInclusionZone(p))
+            .ToArray();
         _tilemap.SetTiles(gridPositions, Enumerable.Repeat(_solidTile, gridPositions.Length).ToArray());
+        _tilemap.SetTiles(outsidePositions, Enumerable.Repeat(_solidTile, outsidePositions.Length).ToArray());
         _meshSurface.BuildNavMesh();
     }
+
+    private bool IsInsideInclusionZone(Vector3Int p)
+        => (p.x + p.y) > -(_excludeLeftRange * 2) && (p.x + p.y) < (_excludeRightRange * 2) && (p.x - p.y) > -(_excludeBackRange * 2) && (p.x - p.y) < (_excludeFrontRange * 2);
 }
